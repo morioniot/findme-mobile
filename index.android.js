c0348fb2d463165c6c82379af4e75cb2e92dc8cd
Miguel Ramos <miguel9ramos@gmail.com>
2016-07-18 21:11:06 -0500

    The library react-native-ble-manager was added to list nearby bluetooth
    The application verifies if the bluetooh is an iBeacon and list all its properties

--- index.android.js
+++ index.android.js
@@ -1,15 +1,110 @@
 import React from 'react'
 import {
     AppRegistry,
-    Text,
-    View,
+    Image,
+    NativeAppEventEmitter,
     ScrollView,
-    Image
+    Text,
+    View
 } from 'react-native'
 
+import BleManager from 'react-native-ble-manager'
 import dogs from './dogs.json'
 
+const toByteString =  function(byte){
+    const byteString = byte.toString(16);
+    if(byteString.length === 1)
+        return '0' + byteString;
+    return byteString;
+};
+
+const toByteArray = function(str){
+    const byteCharacters = atob(str);
+    const byteArray = [];
+    for(let i = 0; i < byteCharacters.length; i++)
+        byteArray.push(byteCharacters.charCodeAt(i));
+    return byteArray;
+};
+
+const toByteStringArray = function(bytesArray){
+    return bytesArray.map(function(byte){
+        return toByteString(byte);
+    });
+};
+
+const validateIBeacon = function(bytes){
+    if(
+        bytes.length > 0 &&
+        bytes[0] === '02' &&
+        bytes[1] === '01' &&
+        bytes[2] === '06' &&
+        bytes[3] === '1a' &&
+        bytes[4] === 'ff' &&
+        bytes[5] === '4c' &&
+        bytes[6] === '00' &&
+        bytes[7] === '02' &&
+        bytes[8] === '15'
+    )
+        return true;
+    return false;
+};
+
+const getUUID = function(bytes){
+    let UUID = "";
+    let UUIDIndex = 9;
+    const UUIDChunkSizes = [4,2,2,2,6];
+    for(let i = 0; i < UUIDChunkSizes.length; i++){
+        for(let j = 0; j < UUIDChunkSizes[i]; j++){
+            UUID += bytes[UUIDIndex];
+            UUIDIndex++;
+        }
+        if(i < UUIDChunkSizes.length - 1)
+            UUID += "-";
+    }
+    return UUID;
+};
+
+const getMajor = function(bytes){
+    const majorHexString = "0x" + bytes[25] + bytes[26];
+    return parseInt(majorHexString);
+};
+
+const getMinor = function(bytes){
+    const minorHexString = "0x" + bytes[27] + bytes[28];
+    return parseInt(minorHexString);
+};
+
+const setBLE = function(){
+    NativeAppEventEmitter.addListener('BleManagerStopScan', function(){
+        console.log("BLE has stopped scanning");
+    });
+    NativeAppEventEmitter.addListener('BleManagerDiscoverPeripheral', function(args){
+        const bytes = toByteArray(args.advertising.data);
+        const hexBytes = toByteStringArray(bytes);
+        const isIBeacon = validateIBeacon(hexBytes);
+        console.log("Is iBeacon?",isIBeacon);
+        console.log("ID:", args.id);
+        console.log("RSSI:", args.rssi);
+        console.log("UUID:", getUUID(hexBytes));
+        console.log("Major:", getMajor(hexBytes));
+        console.log("Minor:", getMinor(hexBytes));
+        console.log("Data:", hexBytes);
+        console.log(args);
+
+    });
+    BleManager.scan([], 5)
+    .then(function(){
+        console.log("BLE has started scanning");
+    })
+    .catch(console.error);
+};
+
 const DogsApp = React.createClass({
+
+    componentDidMount: function(){
+        setBLE();
+    },
+
     render: function(){
         return (
             <View style={{flex:1}}>
